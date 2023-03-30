40fd8b7cc0020d7b1c6fda518a0dbcea906e929d
Elva Rothman <erothman@tulane.edu>
2023-03-30 08:42:46 -0400

    deleted non-working code from conditionals

--- script.js
+++ script.js
@@ -28,8 +28,6 @@ $(function () {
 
     })
 
-    //need to make todos stay on page when page is reloaded 
-
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
     // attribute of each time-block be used to conditionally add or remove the
@@ -37,70 +35,37 @@ $(function () {
     // current hour in 24-hour time?
     //
 
-  
     timeNow = dayjs().format("H")
 
     container = $(".container-lg")
 
     hourIdEl = container.children().data("hour")
 
-    blockBg = $("textarea")
+    blockBg = $(".description")
 
     if (hourIdEl < timeNow) {
       blockBg.attr("class", "past")
+
     } else if (hourIdEl > timeNow) {
       blockBg.attr("class", "future")
+
     }else {
       blockBg.attr("class", "present")
     }
 
-    console.log(hourIdEl)
-
-    // for (var i = 0; hourIdEl.length; i++) {
-    //   if (hourIdEl[i] === timeNow) {
-    //     blockBg.attr("class", "present")
-    //     console.log(hourIdEl[i])
-    //   } else if (hourIdEl[i] < timeNow) {
-    //     blockBg.attr("class", "past")
-    //     console.log(hourIdEl[i])
-    //   } else {
-    //     blockBg.attr("class", "future")
-    //     console.log(hourIdEl[i])
-    //   }
-    // }
-
-
-
-    // timeArray = [$("#hour-9"), $("#hour-10"), $("#hour-11"), $("#hour-12"), $("#hour-13"), $("#hour-14"), $("#hour-15"), $("#hour-16"), $("#hour-17")]
-
-
-    // //variable for divs with class="time-block"
-    // timeBlock = $(".time-block")
-
-    // for (var i = 0; i < timeArray.length; i++) {
-    //   if (timeArray[i].text() === timeNow) {
-    //     timeArray[i].attr("class", "present")
-    //   } else if (timeArray[i].text() < timeNow) {
-    //     timeArray[i].attr("class", "past")
-    //   } else {
-    //     timeArray[i].attr("class", "future")
-    //   }
-    // }
-
-    // console.log(dayjs().format("hA"))
-    
-
-
-    
 
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
+
     function currentTime() {
+
       setInterval(function() {
+
       $('#currentDay').text(dayjs().format('MMMM DD, YYYY hh:mm:ss A'))
+
       }, 1000)
       }
     
